221e4d4ce900816880327029406ff591bc74ce36
NeValetik <snowchug@gmail.com>
2024-12-21 14:24:43 +0200

    added sidebar

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,20 +1,27 @@
+'use client'
 import type { Metadata } from "next";
+import React,{ useState } from "react";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/module/Sidebar";
+// "@/components/module/Sidebar"
 
-export const metadata: Metadata = {
-  title: "Hackaton Faf",
-  description: "TEAM HACKATON FAF",
-};
+// export const metadata: Metadata = {
+//   title: "Hackaton Faf",
+//   description: "TEAM HACKATON FAF",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const [sidebarOpen,setSidebarOpen] = useState<boolean>(true) 
   return (
     <html lang="en">
       <body>
+        <Sidebar sidebarOpen = {sidebarOpen} setSidebarOpen={setSidebarOpen}/>
         {children}
       </body>
     </html>
