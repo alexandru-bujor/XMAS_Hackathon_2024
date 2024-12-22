f57533e791f6f404a7280889ae19f7e91706739a
Isacescu Maxim <isacescu.maxim12@gmail.com>
2024-12-22 03:52:36 +0200

    Company entity and commands added

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,11 +8,15 @@ namespace HireMeF.Data
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
 		}
+
 		public DbSet<User> Users { get; set; }
+		public DbSet<Company> Companies { get; set; }
+
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<User>().HasKey(u => u.Id);
+			modelBuilder.Entity<Company>().HasKey(c => c.Id);
 
 		}
 	}
---
+++ UseCases/CompanyCommand/GetCompaniesListQuery.cs
@@ -0,0 +1,42 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using HireMeF.Data;
+using HireMeF.Data.Entities;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HireMeF.UseCases.CompanyCommands
+{
+
+	public class GetNotPaginatedCompaniesListQuery : IRequest<List<Company>>
+	{
+	}
+
+	public class GetNotPaginatedCompanysListQueryHandler : IRequestHandler<GetNotPaginatedCompanysListQuery, List<Company>>
+	{
+		private readonly ApplicationDbContext _appDbContext;
+
+		public GetNotPaginatedCompanyListQueryHandler(ApplicationDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public async Task<List<Company>> Handle(GetNotPaginatedCompanyListQuery request, CancellationToken cancellationToken)
+		{
+			var companies = await _appDbContext.Companies
+				.Select(c => new Company
+				{
+					Id = c.Id,
+					Name = c.Name,
+					Type = c.Type,
+					IsActive = c.IsActive,
+					CreationDate = c.CreationDate,
+                    Address = c.Address,
+                    Turnover = c.Turnvoer
+				}).ToListAsync(cancellationToken);
+
+			return companies;
+		}
+	}
+}
