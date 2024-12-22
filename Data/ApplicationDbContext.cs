6527a6a28bfcbe6d0f4c76a4392e954aba4d6405
Elena <lenaromasenco@gmail.com>
2024-12-22 02:34:52 +0200

    Template

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HireMeF.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HireMeF.Data
+{
+	public class ApplicationDbContext: DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+		public DbSet<User> Users { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<User>().HasKey(u => u.Id);
+
+		}
+	}
+}
---
+++ Data/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace HireMeF.Data.Entities
+{
+	public class User
+	{
+	
+		public Guid Id;
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public DateTime BirthDate { get; set; }
+		public string Studies { get; set; } = string.Empty;
+		public double IDNP { get; set; }
+
+
+	}
+}
