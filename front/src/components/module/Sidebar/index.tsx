ff1b0ec5e33afbc28e4c285878951ddb6dee103b
NeValetik <snowchug@gmail.com>
2024-12-22 02:23:44 +0200

    added account button (needs logo)

--- front/src/components/module/Sidebar/index.tsx
+++ front/src/components/module/Sidebar/index.tsx
@@ -138,46 +138,44 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   // const [pageName, setPageName] = useLocalStorage("selectedMenu", "dashboard");
 
   return (
-    // <ClickOutside onClick={() => setSidebarOpen(false)}>
-      <aside
-        className={`absolute left-0 top-0 flex h-screen w-64 flex-col overflow-y-hidden border-r border-stroke bg-white dark:border-stroke-dark dark:bg-gray-dark lg:static lg:translate-x-0 ${
-          sidebarOpen
-            ? "translate-x-0 duration-300 ease-linear"
-            : "-translate-x-full"
-        }`}
-      >
-        {/* <!-- SIDEBAR HEADER --> */}
-        <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5 xl:py-10">
-          <div className={"items-center"}>
-            <Link href="/">
-              <SiRobotframework className={""} size={40}/>
-            </Link>
-          </div>
-
-
-          <button
-            onClick={() => setSidebarOpen(!sidebarOpen)}
-            className="block lg:hidden"
-          >
-            <svg
-              className="fill-current"
-              width="20"
-              height="18"
-              viewBox="0 0 20 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
-                fill=""
-              />
-            </svg>
-          </button>
+    <aside
+      className={`absolute left-0 top-0 flex h-screen w-64 flex-col overflow-y-hidden border-r border-stroke bg-white dark:border-stroke-dark dark:bg-gray-dark lg:static lg:translate-x-0 ${
+        sidebarOpen
+          ? "translate-x-0 duration-300 ease-linear"
+          : "-translate-x-full"
+      }`}
+    >
+      {/* <!-- SIDEBAR HEADER --> */}
+      <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5 xl:py-10">
+        <div className="items-center">
+          <Link href="/">
+            <SiRobotframework className="" size={40}/>
+          </Link>
         </div>
-        {/* <!-- SIDEBAR HEADER --> */}
 
+        <button
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+          className="block lg:hidden"
+        >
+          <svg
+            className="fill-current"
+            width="20"
+            height="18"
+            viewBox="0 0 20 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
+              fill=""
+            />
+          </svg>
+        </button>
+      </div>
+
+      {/* <!-- Main Navigation --> */}
+      <div className="flex flex-col justify-between h-[calc(100%-88px)]">
         <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
-          {/* <!-- Sidebar Menu --> */}
           <nav className="mt-1 px-4 lg:px-6">
             {menuGroups.map((group, groupIndex) => (
               <div key={groupIndex}>
@@ -190,18 +188,32 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                     <SidebarItem
                       key={menuIndex}
                       item={menuItem}
-                      // pageName={pageName}
-                      // setPageName={setPageName}
                     />
                   ))}
                 </ul>
               </div>
             ))}
           </nav>
-          {/* <!-- Sidebar Menu --> */}
         </div>
-      </aside>
-    // </ClickOutside>
+
+        {/* <!-- Account Section --> */}
+        <div className="px-6 mb-10">
+          <Link 
+            href="/srm/profile" 
+            className="flex items-center hover:bg-gray-100 rounded-lg p-2 transition-colors duration-200"
+          >
+            <Image 
+              src="/logo.svg" 
+              width={16} 
+              height={16} 
+              className="w-5 h-5" 
+              alt="userIcon"
+            />
+            <span className="ml-3 text-gray-700">Account</span>
+          </Link>
+        </div>
+      </div>
+    </aside>
   );
 };
 
